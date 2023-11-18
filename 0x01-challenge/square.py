3bae8ad8e02de02b5e014b41e07f21b370759340
Baddi Mohamed <mehamad199523@gmail.com>
2023-11-18 09:21:14 -0800

    0x00. Fix my code

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" Module for square class"""
+
+
+class Square():
+    """ Square class """
+    width = 0
+    height = 0
+
+    def __init__(self, *args, **kwargs):
+        """_summary_
+        """
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        return self.width * self.height
+
+    def permiter_of_my_square(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        return "{}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+    """_summary_"""
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.permiter_of_my_square())
